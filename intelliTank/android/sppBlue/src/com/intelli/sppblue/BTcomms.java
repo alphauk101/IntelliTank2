8b85e39cf8071a74dcd5c55548fa6d832ce44384
LeeWilliams <alphauk101@hotmail.com>
2014-01-16 16:16:36 +0000

    Update to Android app - starting to take shape.

---
+++ intelliTank/android/sppBlue/src/com/intelli/sppblue/BTcomms.java
@@ -0,0 +1,198 @@
+package com.intelli.sppblue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Message;
+
+
+public class BTcomms {
+	
+	ConnectedThread mConnectedThread;
+	
+	public BTcomms(BluetoothAdapter mBluetoothAdapter)
+	{
+
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		BluetoothDevice deviceHP = null;
+		 for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		        //Log.i(">",device.getName());
+			 	if(device.getAddress().equals("00:80:25:48:01:02"))//address of stollman
+			 	{
+			 		deviceHP = device;
+			 	}
+		    }
+		if(deviceHP != null)
+		{
+			ConnectThread con = new ConnectThread(deviceHP);
+			con.start();
+		}else
+		{
+			sendMessage(MainActivity.ERROR_BLUETOOTH_ADAPTER,"Unable to find the Intelli-tank please ensure you have paired with it.");
+		}
+
+	}
+
+	/*
+	 * Sends a message to the main activities handler
+	 */
+	public void sendMessage(int what, String text)
+	{
+		Message msg = new Message();
+		Bundle bun = new Bundle();
+		msg.what = what;
+		bun.putString(MainActivity.ERROR_MESSAGE_STRING, text);
+		msg.setData(bun);
+		MainActivity.mHandler.sendMessage(msg);
+	}
+	
+	
+	private class ConnectThread extends Thread {
+		
+	    private final BluetoothSocket mmSocket;
+	    private final BluetoothDevice mmDevice;
+	 
+	    public ConnectThread(BluetoothDevice device) {
+	        // Use a temporary object that is later assigned to mmSocket,
+	        // because mmSocket is final
+	        BluetoothSocket tmp = null;
+	        mmDevice = device;
+	        
+	        // Get a BluetoothSocket to connect with the given BluetoothDevice
+	        try {
+	            // MY_UUID is the app's UUID string, also used by the server code
+	        	UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");//SPP UUID
+	            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+	        } catch (IOException e) { }
+	        mmSocket = tmp;
+	    }
+	 
+	    public void run() {
+	        // Cancel discovery because it will slow down the connection
+	        //mBluetoothAdapter.cancelDiscovery();
+	 
+	        try {
+	            // Connect the device through the socket. This will block
+	            // until it succeeds or throws an exception
+	        	if(mmSocket.isConnected() == false)
+	        	{
+	        		mmSocket.connect();
+	        	}
+	        } catch (IOException connectException) {
+	            // Unable to connect; close the socket and get out
+	            try {
+	                mmSocket.close();
+	            } catch (IOException closeException) { }
+	            return;
+	        }
+	 
+	        // Do work to manage the connection (in a separate thread)
+	        manageConnectedSocket(mmSocket);
+	    }
+	 
+	    /** Will cancel an in-progress connection, and close the socket */
+	    public void cancel() {
+	        try {
+	            mmSocket.close();
+	        } catch (IOException e) { }
+	    }
+	}
+
+
+	public void manageConnectedSocket(BluetoothSocket mmSocket)
+	{
+		mConnectedThread = new ConnectedThread(mmSocket);
+		mConnectedThread.start();
+		MainActivity.mHandler.sendEmptyMessage(MainActivity.BLUETOOTH_CONNECTED);
+	}
+
+	//makes cancelling the socket visible
+	public void closeConnection()
+	{
+		if(mConnectedThread != null)
+		{
+			mConnectedThread.cancel();
+		}
+	}
+	
+	public void sendBytesSPP(byte[] dataToSend)
+	{
+		if(mConnectedThread != null)
+		{
+			mConnectedThread.write(dataToSend);
+		}
+	}
+	
+	private class ConnectedThread extends Thread {
+	    private final BluetoothSocket mmSocket;
+	    private final InputStream mmInStream;
+	    private final OutputStream mmOutStream;
+	 
+	    public ConnectedThread(BluetoothSocket socket) {
+	        mmSocket = socket;
+	        InputStream tmpIn = null;
+	        OutputStream tmpOut = null;
+	 
+	        // Get the input and output streams, using temp objects because
+	        // member streams are final
+	        try {
+	            tmpIn = socket.getInputStream();
+	            tmpOut = socket.getOutputStream();
+	        } catch (IOException e) { }
+	 
+	        mmInStream = tmpIn;
+	        mmOutStream = tmpOut;
+	    }
+	 
+	    public void run() {
+	        byte[] buffer = new byte[1024];  // buffer store for the stream
+	        int bytes; // bytes returned from read()
+	 
+	        // Keep listening to the InputStream until an exception occurs
+	        while (true) {
+	            try {
+	                // Read from the InputStream
+	                if(mmInStream.available() > 0)
+	                {
+		            	bytes = mmInStream.read(buffer);
+		            	
+		            	byte[] data = new byte[bytes];
+		            	System.arraycopy(buffer, 0, data, 0, bytes);//copy the data into a sensible sized array
+		            	
+		                // Send the obtained bytes to the UI activity
+		                Message msg = new Message();
+		                Bundle bun = new Bundle();
+		                bun.putByteArray(MainActivity.BLUETOOTH_BYTES_AVAIL_KEY,data);
+		                msg.setData(bun);
+		                MainActivity.mHandler.sendMessage(msg);
+	                }
+	            } catch (IOException e) {
+	                break;
+	            }
+	        }
+	    }
+	 
+	    /* Call this from the main activity to send data to the remote device */
+	    public void write(byte[] bytes) {
+	        try {
+	            mmOutStream.write(bytes);
+	        } catch (IOException e) { }
+	    }
+	 
+	    /* Call this from the main activity to shutdown the connection */
+	    public void cancel() {
+	        try {
+	            mmSocket.close();
+	        } catch (IOException e) { }
+	    }
+	}
+
+}
--- intelliTank/android/sppBlue/src/com/intelli/sppblue/MainActivity.java
+++ intelliTank/android/sppBlue/src/com/intelli/sppblue/MainActivity.java
@@ -1,134 +1,206 @@
+/*
+ * Intelli-Tank 2 app
+ * Will recieve data from tank unit in format of TLV
+ * to be defined-
+ * This is best because I can upgrade and improve on BT transfer without 
+ * affecting the normal data transition.
+ * 
+ * Water temp TLV
+ * type = 0x01;
+ * length = *
+ * value = single unsigned byte (temp should always be around 11 - 35C)
+ */
+
+
 package com.intelli.sppblue;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	BluetoothAdapter mBluetoothAdapter;
 
-	Handler mHandler;
-	ConnectedThread ct;
+	//HANDLER CODES
+	public final static int ERROR_BLUETOOTH_ADAPTER = 73594;
+	public final static String ERROR_MESSAGE_STRING = "errormsg";
+	
+	public final static int BLUETOOTH_BYTES_AVAILABLE = 45813;
+	public final static String BLUETOOTH_BYTES_AVAIL_KEY = "byteavail";
+	
+	public final static int BLUETOOTH_CONNECTED = 54849465;
+	
+	public final static int WATERTEMP_READING = 124565;
+	public final static String WATERTEMP_VALUE = "watertemp";
+	
+	
+	//tlv types
+	private final byte WATERTEMP_TYPE = 0x01;
+
+
+	
+	BluetoothAdapter mBluetoothAdapter;
+	public static Handler mHandler;
+	//ConnectedThread ct;
+	int REQUEST_ENABLE_BT = 10294;
+	BTcomms mBTcomms;
+	
+	static boolean BLUETOOTH_ON = false;//flag to show bluetooth state
+	
+	private TextView tvWaterTemp;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//setup button
+		final Button mButton = (Button)findViewById(R.id.btn_update);
+		mButton.setOnClickListener(new View.OnClickListener() 
+		{	
+			@Override
+			public void onClick(View v) {
+				startBT();				
+			}
+		});
+		mButton.setText("Connect");
+		
 		
 		mHandler = new Handler(){
 			@Override
 			public void handleMessage(Message msg) 
 			{
-				String re = msg.getData().getString("st");
-				Log.i("BT", re);
-				byte[] reply = "Hello from Droid".getBytes();
-				ct.write(reply);
-				//super.handleMessage(msg);
+				switch (msg.what)
+				{
+				case ERROR_BLUETOOTH_ADAPTER:
+						showError(msg.getData().getString(ERROR_MESSAGE_STRING));
+					break;
+				case BLUETOOTH_BYTES_AVAILABLE:
+					//we have some byte here do whatever with them
+					break;
+				case BLUETOOTH_CONNECTED:
+					mButton.setText("Connected");
+					mButton.setEnabled(false);
+					Toast.makeText(getApplicationContext(), "Exit App to disconnect.", Toast.LENGTH_LONG).show();
+					break;
+				case WATERTEMP_READING:
+					int waterTemp = msg.getData().getInt(WATERTEMP_VALUE);
+					//now update the display
+					String wt = String.valueOf(waterTemp) + " \u2103";
+					tvWaterTemp.setText(wt);
+					break;
+				default:
+					break;
+				}
 			}
 		};
-		
-		startBT();
-		
-		
-	}
 
-	
-	private void startBT()
-	{
+		//start the bluetooth actions
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if (mBluetoothAdapter == null) {
-		    Toast.makeText(this, "Nada Bluetooth!", Toast.LENGTH_LONG).show();
+		    //handler
+			
 		}
 		
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-		// If there are paired devices
-		BluetoothDevice deviceHP = null;
-		 for (BluetoothDevice device : pairedDevices) {
-		        // Add the name and address to an array adapter to show in a ListView
-		        //Log.i(">",device.getName());
-			 	if(device.getAddress().equals("00:80:25:48:01:02"))//address of stollman
-			 	{
-			 		deviceHP = device;
-			 	}
-		    }
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}else
+		{
+			BLUETOOTH_ON = true;
+		}
 		
-		ConnectThread con = new ConnectThread(deviceHP);
-		con.start();
+		//setup text inputs for ui
+		initTextInputs();
 		
+		//quick test
+		//byte[] test = new byte[]{0x01,0x01,0x14};
+		
+		//processIncomingData(test);
+	}
+	
+	private void initTextInputs()
+	{
+		tvWaterTemp = (TextView)findViewById(R.id.edt_waterTemp);
+		tvWaterTemp.setText("--C");
 	}
 	
 	
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	private class ConnectThread extends Thread {
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		
-	    private final BluetoothSocket mmSocket;
-	    private final BluetoothDevice mmDevice;
-	 
-	    public ConnectThread(BluetoothDevice device) {
-	        // Use a temporary object that is later assigned to mmSocket,
-	        // because mmSocket is final
-	        BluetoothSocket tmp = null;
-	        mmDevice = device;
-	        
-	        // Get a BluetoothSocket to connect with the given BluetoothDevice
-	        try {
-	            // MY_UUID is the app's UUID string, also used by the server code
-	        	UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");//SPP UUID
-	            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
-	        } catch (IOException e) { }
-	        mmSocket = tmp;
-	    }
-	 
-	    public void run() {
-	        // Cancel discovery because it will slow down the connection
-	        //mBluetoothAdapter.cancelDiscovery();
-	 
-	        try {
-	            // Connect the device through the socket. This will block
-	            // until it succeeds or throws an exception
-	        	if(mmSocket.isConnected() == false)
-	        	{
-	        		mmSocket.connect();
-	        	}
-	        } catch (IOException connectException) {
-	            // Unable to connect; close the socket and get out
-	            try {
-	                mmSocket.close();
-	            } catch (IOException closeException) { }
-	            return;
-	        }
-	 
-	        // Do work to manage the connection (in a separate thread)
-	        manageConnectedSocket(mmSocket);
-	    }
-	 
-	    /** Will cancel an in-progress connection, and close the socket */
-	    public void cancel() {
-	        try {
-	            mmSocket.close();
-	        } catch (IOException e) { }
-	    }
+		if(requestCode == REQUEST_ENABLE_BT)
+		{
+			if(resultCode == RESULT_OK)
+			{
+				//bluetooth is on
+				BLUETOOTH_ON = true;
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 	
+
+	private void startBT()
+	{
+		if(mBluetoothAdapter != null)
+		{
+			mBTcomms = new BTcomms(mBluetoothAdapter);
+		}else
+		{
+			sendMessage(ERROR_BLUETOOTH_ADAPTER, "Problem with your bluetooth adapter.");
+		}
+	}
+	
+	
+	@Override
+	protected void onPause() {
+		if(mBTcomms != null)
+		{
+			mBTcomms.closeConnection();
+		}
+		super.onPause();
+	}
+
+
+	public void sendMessage(int what, String text)
+	{
+		Message msg = new Message();
+		Bundle bun = new Bundle();
+		msg.what = what;
+		bun.putString(ERROR_MESSAGE_STRING, text);
+		msg.setData(bun);
+		mHandler.sendMessage(msg);
+	}
+	
+	private void showError(String error)
+	{
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		adb.setTitle("There was a problem");
+		adb.setMessage(error);
+		adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		adb.create().show();
+	}
+	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -137,73 +209,55 @@ public class MainActivity extends Activity {
 	}
 
 
-	public void manageConnectedSocket(BluetoothSocket mmSocket)
+	//here we process our incoming data
+	//calls the handler with the appropriate action
+	private void processIncomingData(byte[] data)
 	{
-		ct = new ConnectedThread(mmSocket);
-		ct.run();
+		if((data != null) && (data.length > 0))//due diligence on data
+		{
+			//get the first byte
+			int cursor = 0;
+			while(cursor < data.length)
+			{
+				//go round our data
+				byte type = data[cursor];
+				cursor++;
+				byte length = data[cursor];
+				cursor ++;
+				//now use the length to get the relevant bytes
+				byte[] TLVdata = new byte[length];
+				for(int a = 0; a < length; a++)
+				{
+					TLVdata[a] = data[cursor];
+					cursor++;
+				}
+				//here we should have the type, length, data AND a cursor thats in the right place to process the next lot of data
+				//but process this chunk first 
+				processChunk(type, TLVdata);
+				
+			}
+		}//else we dont care
 	}
 
-	private class ConnectedThread extends Thread {
-	    private final BluetoothSocket mmSocket;
-	    private final InputStream mmInStream;
-	    private final OutputStream mmOutStream;
-	 
-	    public ConnectedThread(BluetoothSocket socket) {
-	        mmSocket = socket;
-	        InputStream tmpIn = null;
-	        OutputStream tmpOut = null;
-	 
-	        // Get the input and output streams, using temp objects because
-	        // member streams are final
-	        try {
-	            tmpIn = socket.getInputStream();
-	            tmpOut = socket.getOutputStream();
-	        } catch (IOException e) { }
-	 
-	        mmInStream = tmpIn;
-	        mmOutStream = tmpOut;
-	    }
-	 
-	    public void run() {
-	        byte[] buffer = new byte[1024];  // buffer store for the stream
-	        int bytes; // bytes returned from read()
-	 
-	        // Keep listening to the InputStream until an exception occurs
-	        while (true) {
-	            try {
-	                // Read from the InputStream
-	                if(mmInStream.available() > 0)
-	                {
-		            	bytes = mmInStream.read(buffer);
-		            	//mmOutStream.write(0x30);
-		            	
-		                // Send the obtained bytes to the UI activity
-		                String m = new String(buffer);
-		                Message msg = new Message();
-		                Bundle bun = new Bundle();
-		                bun.putString("st", m);
-		                msg.setData(bun);
-		                mHandler.sendMessage(msg);
-	                }
-	            } catch (IOException e) {
-	                break;
-	            }
-	        }
-	    }
-	 
-	    /* Call this from the main activity to send data to the remote device */
-	    public void write(byte[] bytes) {
-	        try {
-	            mmOutStream.write(bytes);
-	        } catch (IOException e) { }
-	    }
-	 
-	    /* Call this from the main activity to shutdown the connection */
-	    public void cancel() {
-	        try {
-	            mmSocket.close();
-	        } catch (IOException e) { }
-	    }
+	private void processChunk(byte type, byte[] data)
+	{
+		//first check we have a type#
+		switch (type)
+		{
+		case WATERTEMP_TYPE:
+			//we have the water temp reading
+			//so the data should be just one byte
+			int waterTmp = (int)data[0];
+			Message msg = new Message();
+			msg.what = WATERTEMP_READING;
+			Bundle bun = new Bundle();
+			bun.putInt(WATERTEMP_VALUE, waterTmp);
+			msg.setData(bun);
+			mHandler.sendMessage(msg);
+			break;
+
+		default:
+			break;
+		}
 	}
-	
 }
